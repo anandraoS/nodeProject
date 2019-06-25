ccd0fd96a6ae6af03bb4fe25a5c720fe3052ce7b
anandraoS <sahukari.anandarao@gmail.com>
2019-06-25 21:32:03 +0530

    enum and custom validators

--- validation.js
+++ validation.js
@@ -3,13 +3,24 @@ mongoose.connect('mongodb://localhost/mongo-exercises')
 .then(()=> console.log('connection estableshed'))
 .catch(err=> console.error('connection failed',err));
 const courseSchema = mongoose.Schema({
-  name: {type: String , required: true},
+  name: {type: String ,
+     required: true,
+     minlength: 5,
+     maxlength: 23
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web','network','appDev']
+    },
   author: String,
   tags: [String],
   date: { type:Date, default: Date.now },
   isPublished: Boolean,
   price: {
       type: Number,
+      min :10,
+      max: 250,
       required : function(){return this.isPublished;}
   }
 });
@@ -18,8 +29,10 @@ async function createCourse() {
   const course = new Course({
     name: "NOde JS coursse",
     author: "anand",
+    category: '-',
     tags: ["node", "express"],
-    isPublished: true
+    isPublished: true,
+    price: 23
   });
   try{
     const result = await course.save();
