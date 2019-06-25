5863b228e123b01ecb96549b3debfa93b22e5b27
anandraoS <sahukari.anandarao@gmail.com>
2019-06-25 21:19:57 +0530

    validation of name

---
+++ validation.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mongo-exercises')
+.then(()=> console.log('connection estableshed'))
+.catch(err=> console.error('connection failed',err));
+const courseSchema = mongoose.Schema({
+  name: {type: String , required: true},
+  author: String,
+  tags: [String],
+  date: { type:Date, default: Date.now },
+  isPublished: Boolean
+});
+const Course = mongoose.model("Course", courseSchema);
+async function createCourse() {
+  const course = new Course({
+    //  name: "NOde JS coursse",
+    author: "anand",
+    tags: ["node", "express"],
+    isPublished: true
+  });
+  try{
+    const result = await course.save();
+    console.log(result);
+  }
+  catch(ex){
+      console.log(ex.message);
+  }
+}
+
+createCourse();
