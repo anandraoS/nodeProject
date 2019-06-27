bf050489d17595fc28cefe90b2051226df6da950
anandraoS <sahukari.anandarao@gmail.com>
2019-06-27 23:41:53 +0530

    Object validation using mongoos

--- genres/routes/rental.js
+++ genres/routes/rental.js
@@ -15,7 +15,9 @@ router.get("./", async (req, res) => {
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) res.status(400).send(error.details[0].message);
-
+  if (!mongoose.Types.ObjectId.isValid(req.body.customerId))
+    return res.status(400).send("Invalid Customer");
+    
   const customer = Customer.findById(req.body.customerId);
   if (!customer) return res.status(400).send("Invalid Customer");
 
