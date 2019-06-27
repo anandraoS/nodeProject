7ac1da025c70909f891e0125ad3789a671887748
anandraoS <sahukari.anandarao@gmail.com>
2019-06-28 00:03:01 +0530

    just working

--- movieRental/models/movie.js
+++ movieRental/models/movie.js
@@ -1,4 +1,3 @@
-const Joi = require('joi');
 const mongoose = require('mongoose');
 const {genreSchema} = require('./genre');
 
@@ -31,7 +30,7 @@ const Movie = mongoose.model('Movies', new mongoose.Schema({
 function validateMovie(movie) {
   const schema = {
     title: Joi.string().min(5).max(50).required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).required(),
     dailyRentalRate: Joi.number().min(0).required()
   };
