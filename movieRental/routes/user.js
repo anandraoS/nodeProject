ca430859f2b243a23d394bf8ed0bdc1696176d3d
anandraoS <sahukari.anandarao@gmail.com>
2019-07-02 23:15:55 +0530

    lodash

--- movieRental/routes/user.js
+++ movieRental/routes/user.js
@@ -1,4 +1,5 @@
 const _ = require("lodash");
+const bcrypt = require('bcrypt');
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
@@ -10,6 +11,9 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already exists");
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
   user = await user.save();
 
   res.send(_.pick(user, ["name", "email", "_id"]));
