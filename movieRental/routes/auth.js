1bb3b94ed339893a9abaf6c9f70b2b797969567a
anandraoS <sahukari.anandarao@gmail.com>
2019-07-03 00:05:31 +0530

    config libraries and hiding the secret keys in environment variables

---
+++ movieRental/routes/auth.js
@@ -0,0 +1,36 @@
+const config = require("config");
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const Joi = require("Joi");
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) res.status(400).send("Invalid email or password");
+  
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  res.send(token);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string()
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(req, schema);
+}
+module.exports = router;
