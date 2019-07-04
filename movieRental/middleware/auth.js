d5e97336f34f01b6fe1e5f40991c23853878bb60
anandraoS <sahukari.anandarao@gmail.com>
2019-07-04 22:21:55 +0530

    protecting routes with middleware

--- movieRental/middleware/auth.js
+++ movieRental/middleware/auth.js
@@ -3,10 +3,13 @@ const config = require("config");
 
 module.exports = function auth(req, res, next) {
   const token = req.header("x-auth-token");
+  console.log(token);
   if (!token) return res.status(401).send("Access denied no token provided");
   try {
     const payload = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = payload;
     next();
-  } catch (exc) {}
+  } catch (exc) {
+      res.status(400).send('Invalid Token');
+  }
 };
