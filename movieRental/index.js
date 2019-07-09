f9c56c615ca85926f3b8c79ba37cccf3f7dcb676
anandraoS <sahukari.anandarao@gmail.com>
2019-07-09 22:38:17 +0530

    extracting and seperating all the logic in startup

--- movieRental/index.js
+++ movieRental/index.js
@@ -1,24 +1,12 @@
-
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
+const winston = require('winston');
 const express = require("express");
 const app = express();
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/logging");
+require("./startup/logging")();
+require("./startup/config")();
+require('./startup/validation')();
 // throw new Error('Something failed during startup');
-process.on("unhandledRejection", ex => {
-  throw ex;
-});
 
-const p = Promise.reject(new Error("something failed miserably"));
-
-p.then(() => console.log("done"));
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
-}
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
