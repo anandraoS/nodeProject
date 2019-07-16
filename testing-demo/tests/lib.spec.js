c4708a15a1e62a4144ad7287920b8d1123418388
anandraoS <sahukari.anandarao@gmail.com>
2019-07-17 00:01:59 +0530

    implementation changed

--- testing-demo/tests/lib.spec.js
+++ testing-demo/tests/lib.spec.js
@@ -1,13 +1,15 @@
 const lib = require('../lib');
-it('absolute - should return a positive number if input is positive', ()=>{
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
-it('absolute - should return a positive number if input is negative', ()=>{
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
-it('absolute - should return a 0 if input is 0', ()=>{
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+describe('absolute',()=>{
+    it('should return a positive number if input is positive', ()=>{
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    it('should return a positive number if input is negative', ()=>{
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    it('should return a 0 if input is 0', ()=>{
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
