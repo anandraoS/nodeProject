ee5f7015236f657893f2d4a3e332093939e4a14e
anandraoS <sahukari.anandarao@gmail.com>
2019-07-16 23:37:59 +0530

    absolute method is tested in one scenario

--- testing-demo/tests/lib.spec.js
+++ testing-demo/tests/lib.spec.js
@@ -1,3 +1,5 @@
-test('our first test', ()=>{
-    throw new Error('Something Failed');
+const lib = require('../lib');
+test('absolute - should return a positive number if input is positive', ()=>{
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
