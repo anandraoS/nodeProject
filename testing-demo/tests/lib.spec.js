de669d9b5a3dc0318ca35bb3a128d115dd6eede4
anandraoS <sahukari.anandarao@gmail.com>
2019-07-16 23:53:41 +0530

    we can use it also instead of test

--- testing-demo/tests/lib.spec.js
+++ testing-demo/tests/lib.spec.js
@@ -1,13 +1,13 @@
 const lib = require('../lib');
-test('absolute - should return a positive number if input is positive', ()=>{
+it('absolute - should return a positive number if input is positive', ()=>{
     const result = lib.absolute(1);
     expect(result).toBe(1);
 });
-test('absolute - should return a positive number if input is negative', ()=>{
+it('absolute - should return a positive number if input is negative', ()=>{
     const result = lib.absolute(-1);
     expect(result).toBe(1);
 });
-test('absolute - should return a 0 if input is 0', ()=>{
+it('absolute - should return a 0 if input is 0', ()=>{
     const result = lib.absolute(0);
     expect(result).toBe(0);
 });
