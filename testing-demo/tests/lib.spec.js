0d4eab38dca5abceba0f509027dea4deb1970089
anandraoS <sahukari.anandarao@gmail.com>
2019-07-17 00:14:16 +0530

    testing strings with pattern and containing

--- testing-demo/tests/lib.spec.js
+++ testing-demo/tests/lib.spec.js
@@ -1,15 +1,23 @@
-const lib = require('../lib');
-describe('absolute',()=>{
-    it('should return a positive number if input is positive', ()=>{
-        const result = lib.absolute(1);
-        expect(result).toBe(1);
-    });
-    it('should return a positive number if input is negative', ()=>{
-        const result = lib.absolute(-1);
-        expect(result).toBe(1);
-    });
-    it('should return a 0 if input is 0', ()=>{
-        const result = lib.absolute(0);
-        expect(result).toBe(0);
-    });
+const lib = require("../lib");
+describe("absolute", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+  it("should return a 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("Anand");
+    expect(result).toMatch(/Anand/);
+    expect(result).toContain('Anand');
+    
+  });
 });
