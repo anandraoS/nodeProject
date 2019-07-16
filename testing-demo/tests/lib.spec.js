ed7e7502418d2e1d71fc149ee8165d48b1ddaca1
anandraoS <sahukari.anandarao@gmail.com>
2019-07-16 23:43:08 +0530

    three test cases

--- testing-demo/tests/lib.spec.js
+++ testing-demo/tests/lib.spec.js
@@ -3,3 +3,11 @@ test('absolute - should return a positive number if input is positive', ()=>{
     const result = lib.absolute(1);
     expect(result).toBe(1);
 });
+test('absolute - should return a positive number if input is negative', ()=>{
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+test('absolute - should return a 0 if input is 0', ()=>{
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+});
